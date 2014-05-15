363192f958073427b64dc37273cc7f0642568655
WindAzure <cvbnm0002@yahoo.com.tw>
2014-05-15 18:36:19 +0800

    Add ManagerForm

---
+++ DataBaseProject/DataBaseProject/Forms/ManagerHomeForm.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBaseProject.Forms
+{
+    /// <summary>
+    /// Interaction logic for ManagerHomeForm.xaml
+    /// </summary>
+    public partial class ManagerHomeForm : UserControl
+    {
+        private void DrawTitle()
+        {
+            _title.Children.Clear();
+            double height = _title.ActualHeight;
+            double width = _title.ActualWidth;
+            for (double i = 10.0; i < width; i += 10.0)
+            {
+                Line line = new Line();
+                line.X1 = i;
+                line.Y1 = 0.0;
+                line.X2 = i - 10.0;
+                line.Y2 = height - 10;
+                line.StrokeThickness = 0.5;
+                line.Stroke = Brushes.Black;
+                _title.Children.Add(line);
+            }
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            DrawTitle();
+        }
+
+        public ManagerHomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickBackButton(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ClickCloseButton(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
--- DataBaseProject/DataBaseProject/MainWindow.xaml.cs
+++ DataBaseProject/DataBaseProject/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace DataBaseProject
         {
             InitializeComponent();
             PageSwitcher._maindow = this;
-            PageSwitcher.Switch(new LoginForm());
+            PageSwitcher.Switch(new ManagerHomeForm());
         }
 
         public void Navigate(UserControl control)
